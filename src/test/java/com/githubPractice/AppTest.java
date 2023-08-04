5c767362c8c9129259817f0b2a0a8bd8bf775ff0
rajesh bommidi <rajesh.bommidi@oracle.com>
2023-08-04 11:44:09 +0530

    first commit inmaster

---
+++ src/test/java/com/githubPractice/AppTest.java
@@ -0,0 +1,20 @@
+package com.githubPractice;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
